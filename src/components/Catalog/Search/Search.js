539d306f0cfa07b1d6fd352d395a716a2e8dac37
chrisbgvt <kristiqn.georgiev.bg@gmail.com>
2022-08-11 23:58:16 +0300

    clean imports

--- src/components/Catalog/Search/Search.js
+++ src/components/Catalog/Search/Search.js
@@ -1,16 +1,10 @@
-import { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { Formik, Form } from 'formik';
-import { Container, Row, Col, Button, Alert } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import * as Yup from 'yup';
 
-// import { AuthContext } from "../../contexts/AuthContext";
 import TextField from "../../Inputs/TextField";
 
 const Search = ({search}) => {
-    // const { userLogin } = useContext(AuthContext);
-    const navigate = useNavigate();
-    const [flag, setFlag] = useState({text: '', check: false});
     const validate = Yup.object().shape({
         query: Yup.string()
             .min(3, 'type at lest 3 symbols')
@@ -41,7 +35,6 @@ const Search = ({search}) => {
                             resetForm
                         }) => (
                             <Form>
-                                {flag.check && <Alert variant="danger">{flag.text}</Alert>}
                                 <TextField name="query" placeholder="Search..." type="text" onChange={handleChange} value={values.query} />
 
                                 <div className={'d-flex justify-content-between'}>
