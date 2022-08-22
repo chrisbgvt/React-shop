03a06c845fe574d33fd3af88a1d40a6448d4b195
chrisbgvt <kristiqn.georgiev.bg@gmail.com>
2022-08-22 20:55:26 +0300

    select field improvments

--- src/components/Inputs/SelectField.js
+++ src/components/Inputs/SelectField.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { ErrorMessage, useField } from 'formik';
+import { Form } from 'react-bootstrap';
 
 const SelectField = ({ label, ...props }) => {
     const [field, meta] = useField(props);
     return (
         <div className="mb-2">
             <label htmlFor={field.name}>{label}</label>
-            <select 
+            <Form.Select 
                 className={`form-control ${meta.touched && meta.error && 'is-invalid'}`} 
                 {...field} {...props}
                 aria-label="Default select example"
             >
                 <option value="user">User</option>
                 <option value="admin">Admin</option>
-            </select>
+            </Form.Select>
             <ErrorMessage component="div" name={field.name} className="text-danger" />
         </div>
     )
