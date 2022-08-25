945b12eb05d0f058b60dbb0e41ec77ca9f42cac0
chrisbgvt <kristiqn.georgiev.bg@gmail.com>
2022-08-25 20:20:56 +0300

    orders table

--- src/components/Profile/OrderTable/OrderTable.js
+++ src/components/Profile/OrderTable/OrderTable.js
@@ -7,7 +7,7 @@ const OrderTable = ({userOrders, ordersPerPage, totalOrders, currentPage, setCur
             <Table striped bordered hover data-aos="fade-up">
                 <thead>
                     <tr>
-                        <th>#</th>
+                        <th>ID</th>
                         <th>Ordered products</th>
                         <th>Total price</th>
                     </tr>
@@ -15,7 +15,7 @@ const OrderTable = ({userOrders, ordersPerPage, totalOrders, currentPage, setCur
                 <tbody>
                     {userOrders.map((x, index) => 
                         <tr key={x._id}>
-                            <td>{index + 1}</td>
+                            <td>{x._id}</td>
                             <td>{x.titles.join(', ')}</td>
                             <td>{x.totalPrice} lv.</td>
                         </tr>
